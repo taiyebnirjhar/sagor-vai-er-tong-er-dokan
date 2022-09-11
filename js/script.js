c6ff8cb2aa3a9882adab0cb5d3b4f54daa2ecc22
taiyebnirjhar <nirjhar.212902003.green.ac.bd>
2022-09-11 18:13:52 +0600

    dynamic cart add funtionality added

--- js/script.js
+++ js/script.js
@@ -1,29 +1,90 @@
-let navbar = document.querySelector('.navbar');
+/*******************************************/
+/* DEFAULT */
+// navbar 4 css
+let navbar = document.querySelector(".navbar");
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
+// search form
+let searchForm = document.querySelector(".search-form");
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+  navbar.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
+// cart Item 4 css
+let cartItem = document.querySelector(".cart-items-container");
+const defaultItem = document.querySelector("#default-item");
+
+document.querySelector("#cart-btn").onclick = () => {
+  cartItem.classList.toggle("active");
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+};
+
+// onscroll 4 css
+window.onscroll = () => {
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+/*******************************************/
+
+/* DYNAMIC */
+//get element
+const checkNow = document.querySelector("#check_now");
+
+const dudhCha = document.querySelector("#dudh_cha a");
+const rongCha = document.querySelector("#rong_cha a");
+const horlicsCha = document.querySelector("#horlics_cha a");
+const coffeeCha = document.querySelector("#coffee_cha a");
+const lebuCha = document.querySelector("#lebu_cha");
+const monerCha = document.querySelector("#moner_cha");
+// functions
+function newOrder(name, price, img) {
+  const div = document.createElement("div");
+  div.classList.add("cart-item");
+
+  div.innerHTML = `
+  
+        <span class="fas fa-times"></span>
+        <img src="${img}" alt="" />
+        <div class="content">
+          <h3>${name}</h3>
+          <div class="price"  style="font-weight: 900">${price}</div>
+        </div>
+
+    `;
+  cartItem.insertBefore(div, checkNow);
 }
 
-let searchForm = document.querySelector('.search-form');
+function addOrder(e) {
+  const parent = e.target.parentElement;
+  const img = parent.childNodes[1].getAttribute("src");
+  const name = parent.childNodes[3].textContent;
+  const price = parent.childNodes[5].firstChild.textContent;
 
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
-    cartItem.classList.remove('active');
+  newOrder(name, price, img);
+}
+function onClick(name) {
+  name.addEventListener("click", (e) => {
+    addOrder(e);
+    console.log("clicked");
+  });
 }
 
-let cartItem = document.querySelector('.cart-items-container');
+//apend
+onClick(dudhCha);
+onClick(rongCha);
+onClick(horlicsCha);
+onClick(coffeeCha);
+onClick(lebuCha);
+onClick(monerCha);
 
-document.querySelector('#cart-btn').onclick = () =>{
-    cartItem.classList.toggle('active');
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-}
-
-window.onscroll = () =>{
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
-}
+// card container
